9e5be4732a760c8811254fdbbc9ce80776e4a488
fortydeea <40deea@gmail.com>
2026-09-18 15:40:37 -0700

    Add overrides and adjustments
    Add overrides and adjustments for better visbility

--- colors/base16-40deea.lua
+++ colors/base16-40deea.lua
@@ -4,11 +4,12 @@ if vim.fn.exists("syntax_on") == 1 then
 end
 vim.g.colors_name = "base16-40deea"
 
+-- dfdsf
 require("base16-colorscheme").setup({
 	base00 = "#191919",
-	base01 = "#404040",
-	base02 = "#061921",
-	base03 = "#5C6B70",
+	base01 = "#273338",
+	base02 = "#273338",
+	base03 = "#667680",
 	base04 = "#DDDDDD",
 	base05 = "#D3DBDE",
 	base06 = "#091997",
@@ -22,3 +23,30 @@ require("base16-colorscheme").setup({
 	base0E = "#E2894A",
 	base0F = "#D3DBDE",
 })
+
+-- Indentation override
+vim.api.nvim_set_hl(0, "IblIndent", { fg = "#40DEEA" })
+vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = "#40DEEA" })
+
+-- Status line override
+vim.api.nvim_set_hl(0, "StatusLine", { fg = "#0a0a0c", bg = "#40DEEA" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#40DEEA", bg = "#273338" })
+
+-- Mini status line overrides
+vim.api.nvim_set_hl(0, "MiniStatuslineModeNormal", { fg = "#40DEEA", bg = "#172A38" })
+vim.api.nvim_set_hl(0, "MiniStatuslineModeInsert", { fg = "#40DEEA", bg = "#1C2126" })
+vim.api.nvim_set_hl(0, "MiniStatuslineModeVisual", { fg = "#40DEEA", bg = "#331C1C" })
+vim.api.nvim_set_hl(0, "MiniStatuslineModeReplace", { fg = "#40DEEA", bg = "#332B14" })
+vim.api.nvim_set_hl(0, "MiniStatuslineModeCommand", { fg = "#40DEEA", bg = "#241A33" })
+vim.api.nvim_set_hl(0, "MiniStatuslineModeOther", { fg = "#40DEEA", bg = "#143330" })
+
+-- Message area override
+vim.api.nvim_set_hl(0, "MsgArea", { fg = "#40DEEA" })
+
+-- neo-tree override
+vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#E0A25C", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#7EC488", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#7EC488", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#D97B70", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = "#D97B70", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#C9A0D5" })
